08d739ec54cd575316c6a3c8ed3a34f6a883922e
Hackerman-808 <addressforitstuff@gmail.com>
2024-11-14 15:53:32 +0100

    Added alphabetical character counting, as well as overhauling and rewriting all other functions. Nice and tidy now.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-def main():
+def readtext():
+    # This is a one-liner option, but poses risks as without with statement things can complicate:
+    # return open("books/frankenstein.txt").read()
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        return file_contents
+    return file_contents
+
+    
+def formatword():
+    return (readtext().split())
+    
+    
+def formatchar():
+    return "".join([char for char in readtext().lower() if char.isalpha()])
 
 
 def wordcount():
-    return len((main().split()))
+    return len(formatword())
 
 
-print(wordcount())
+def charcount():
+    return {char: formatchar().count(char) for char in set(formatchar())}
+
+
+def main():
+    print(f"Number of words is: {wordcount()} \n{'\n'.join(f'{k}: {v}' for k, v in charcount().items())}")
+
+
+main()
